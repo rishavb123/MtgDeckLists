a91fa8feaf548767042d389a9c066f001c236791
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-06-09 19:58:37 -0400

    added build

---
+++ precache-manifest.0ac37d79a0341c64354db9436e236a09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ba8a42ec5062be7da5f52fc7e17fb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b35181290b27443da260",
+    "url": "/static/css/main.bc9c260e.chunk.css"
+  },
+  {
+    "revision": "afe720bfc6710ab820fe",
+    "url": "/static/js/2.b87f088a.chunk.js"
+  },
+  {
+    "revision": "b35181290b27443da260",
+    "url": "/static/js/main.e794a4e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
